0297b55c46bef09c5269379d39463d3b19aa19a5
efecansamsa2005 <efecansamsa2005@gmail.com>
2025-12-10 19:43:04 +0300

    Fix function

--- netlify/functions/places.js
+++ netlify/functions/places.js
@@ -1,3 +1,5 @@
+const https = require('https');
+
 const GOOGLE_API_KEY = 'AIzaSyDO7obcIDXDAw-ga891GIwUFRB_oc9OT-c';
 
 exports.handler = async (event) => {
@@ -14,21 +16,33 @@ exports.handler = async (event) => {
   try {
     const { query, city } = JSON.parse(event.body);
     
-    const response = await fetch('https://places.googleapis.com/v1/places:searchText', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Goog-Api-Key': GOOGLE_API_KEY,
-        'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.priceLevel,places.photos,places.regularOpeningHours,places.editorialSummary'
-      },
-      body: JSON.stringify({
-        textQuery: `${query} in ${city}`,
-        maxResultCount: 6,
-        languageCode: 'en'
-      })
+    const requestBody = JSON.stringify({
+      textQuery: `${query} in ${city}`,
+      maxResultCount: 6,
+      languageCode: 'en'
     });
 
-    const data = await response.json();
+    const data = await new Promise((resolve, reject) => {
+      const req = https.request({
+        hostname: 'places.googleapis.com',
+        path: '/v1/places:searchText',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Goog-Api-Key': GOOGLE_API_KEY,
+          'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.rating,places.userRatingCount,places.priceLevel,places.photos,places.regularOpeningHours,places.editorialSummary',
+          'Content-Length': Buffer.byteLength(requestBody)
+        }
+      }, (res) => {
+        let body = '';
+        res.on('data', chunk => body += chunk);
+        res.on('end', () => resolve(JSON.parse(body)));
+      });
+      req.on('error', reject);
+      req.write(requestBody);
+      req.end();
+    });
+
     return { statusCode: 200, headers, body: JSON.stringify(data) };
   } catch (error) {
     return { statusCode: 500, headers, body: JSON.stringify({ error: error.message }) };
